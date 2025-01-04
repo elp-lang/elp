a9ccf8d272d8ac09f05e5eefcb939af4030c6548
davemackintosh <me@davemackintosh.co.uk>
2025-01-04 16:30:13 +0000

    Added tests for elp type AST

--- crates/parser/src/ast/elp_type.rs
+++ crates/parser/src/ast/elp_type.rs
@@ -23,3 +23,79 @@ pub struct ElpTypeGeneric {
 pub struct ElpTypeGenericConstraint {
     pub constraints: Vec<ElpType>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::parser::ElpParser;
+    use from_pest::FromPest;
+    use pest::Parser;
+
+    #[test]
+    fn elp_type_ast_generation() {
+        let expression_str = "String";
+        let mut pairs = ElpParser::parse(Rule::elp_type, expression_str).unwrap();
+        let ast = ElpType::from_pest(&mut pairs).unwrap();
+
+        assert_eq!(
+            ast,
+            ElpType {
+                name: "String".into(),
+                type_parameters: vec![]
+            }
+        )
+    }
+
+    #[test]
+    fn elp_generic_ast_generation() {
+        let expression_str = "<String: Copy>";
+        let mut pairs = ElpParser::parse(Rule::elp_generic, expression_str).unwrap();
+        let ast = ElpTypeGeneric::from_pest(&mut pairs).unwrap();
+
+        assert_eq!(
+            ast,
+            ElpTypeGeneric {
+                elp_type: ElpType {
+                    name: "String".into(),
+                    type_parameters: vec![]
+                },
+                type_constraint: Some(ElpTypeGenericConstraint {
+                    constraints: vec![ElpType {
+                        name: "Copy".into(),
+                        type_parameters: vec![]
+                    }]
+                })
+            }
+        )
+    }
+
+    #[test]
+    fn elp_generic_constraint_ast_generation() {
+        let expression_str = "<String: Copy + Clone>";
+        let mut pairs = ElpParser::parse(Rule::elp_generic, expression_str).unwrap();
+        let ast = ElpTypeGeneric::from_pest(&mut pairs).unwrap();
+
+        assert_eq!(
+            ast,
+            ElpTypeGeneric {
+                elp_type: ElpType {
+                    name: "String".into(),
+                    type_parameters: vec![]
+                },
+                type_constraint: Some(ElpTypeGenericConstraint {
+                    constraints: vec![
+                        ElpType {
+                            name: "Copy".into(),
+                            type_parameters: vec![]
+                        },
+                        ElpType {
+                            name: "Clone".into(),
+                            type_parameters: vec![]
+                        }
+                    ]
+                })
+            }
+        )
+    }
+}
