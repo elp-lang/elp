e7c909f3265c952baa22203db53d55aef3d27123
davemackintosh <me@davemackintosh.co.uk>
2025-08-03 08:28:59 +0100

    basic lexer coming together

--- crates/lexer/src/lib.rs
+++ crates/lexer/src/lib.rs
@@ -7,16 +7,14 @@ pub mod tokens;
 
 pub struct ElpLexer {
     input: String,
-    start: usize,
     cursor: usize,
     line: usize,
 }
 
-impl<'l> ElpLexer {
+impl ElpLexer {
     pub fn new(input: String) -> Self {
         Self {
             input,
-            start: 0,
             cursor: 0,
             line: 1,
         }
@@ -30,21 +28,11 @@ impl<'l> ElpLexer {
         self.cursor += 1;
     }
 
-    fn put_back(&mut self) {
-        self.cursor -= 1;
-    }
-
     fn peek_next(&self) -> char {
         if self.cursor + 1 >= self.input.len() {
             return '\0';
         }
         self.input.chars().nth(self.cursor + 1).unwrap()
-    }
-
-    fn skip_whitespace(&mut self) {
-        while !self.is_at_end() && self.peek_next().is_whitespace() {
-            self.consume()
-        }
     }
 
     fn consume_identifier(&mut self) -> SpannedToken {
@@ -59,15 +47,22 @@ impl<'l> ElpLexer {
         let mut value: String = "".into();
 
         loop {
-            let char = self.peek_next();
-            if char.is_ascii_whitespace() || !char.is_ascii_alphanumeric() || char != '_' {
+            if self.is_at_end() {
+                break;
+            }
+            let char = self.input.chars().nth(self.cursor).unwrap();
+            if self.is_at_end() || char.is_whitespace() || (!char.is_alphanumeric() && char != '_')
+            {
+                dbg!(self.is_at_end(), char);
+                self.consume();
                 break;
             }
 
             self.consume();
-            value.push(self.input.chars().nth(self.cursor).unwrap());
+            value.push(char);
         }
         token.span.end = self.cursor;
+        token.span.source = value.clone();
         token.token = LexerTokens::Identifier(value);
 
         token
@@ -85,21 +80,53 @@ impl<'l> ElpLexer {
         let mut value: String = "".into();
 
         loop {
-            let char = self.peek_next();
-            if char.is_ascii_whitespace() || !char.is_numeric() || char != '_' {
+            if self.is_at_end() {
+                break;
+            }
+            let char = self.input.chars().nth(self.cursor).unwrap();
+            if self.is_at_end()
+                || char.is_whitespace()
+                || (!char.is_numeric() && char != '_' && char != '.')
+            {
                 break;
             }
 
             self.consume();
-            value.push(self.input.chars().nth(self.cursor).unwrap());
+            value.push(char);
         }
         token.span.end = self.cursor;
+        token.span.source = value.clone();
         token.token = LexerTokens::Number(value);
 
         token
     }
 
-    fn consume_symbols(&mut self) -> SpannedToken {}
+    fn consume_symbols(&mut self) -> SpannedToken {
+        let mut token = SpannedToken {
+            span: CodeSpan {
+                start: self.cursor,
+                end: self.cursor,
+                source: "".into(),
+            },
+            token: LexerTokens::Identifier("".into()),
+        };
+        let mut value: String = "".into();
+
+        loop {
+            let char = self.peek_next();
+            if self.is_at_end() || char.is_whitespace() || char.is_alphanumeric() {
+                break;
+            }
+
+            self.consume();
+            value.push(char);
+        }
+
+        token.span.end = self.cursor;
+        token.token = LexerTokens::Symbol(value);
+
+        token
+    }
 
     fn scan(&mut self) -> Vec<SpannedToken> {
         let mut tokens = vec![SpannedToken {
@@ -112,15 +139,21 @@ impl<'l> ElpLexer {
         }];
 
         while !self.is_at_end() {
-            let next_char = self.peek_next();
+            let char = self.input.chars().nth(self.cursor).unwrap();
 
-            if next_char == '\0' {
+            if char == '\0' {
                 break;
             }
 
-            if next_char.is_alphabetic() || next_char == '_' {
+            if char.is_whitespace() {
+                if char == 0xA as char {
+                    self.line += 1;
+                }
+                self.consume();
+                continue;
+            } else if char.is_alphabetic() || char == '_' {
                 tokens.push(self.consume_identifier());
-            } else if next_char.is_numeric() {
+            } else if char.is_numeric() {
                 tokens.push(self.consume_numeric());
             } else {
                 tokens.push(self.consume_symbols());
@@ -134,6 +167,7 @@ impl<'l> ElpLexer {
 #[cfg(test)]
 mod test {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_new() {
@@ -145,7 +179,142 @@ mod test {
     #[test]
     fn test_is_at_end() {
         let input = String::from("hello");
-        let lexer = ElpLexer::new(input);
+        let mut lexer = ElpLexer::new(input);
         assert!(!lexer.is_at_end());
+        while !lexer.is_at_end() {
+            lexer.consume();
+        }
+        assert!(lexer.is_at_end());
+    }
+
+    #[test]
+    fn test_scan_identifier() {
+        let input = String::from("hello");
+        let mut lexer = ElpLexer::new(input);
+        let tokens = lexer.scan();
+        assert_eq!(
+            vec![
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 0,
+                        source: "".into()
+                    },
+                    token: LexerTokens::SOI
+                },
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 5,
+                        source: "hello".into()
+                    },
+                    token: LexerTokens::Identifier("hello".into())
+                }
+            ],
+            tokens
+        )
+    }
+
+    #[test]
+    fn test_scan_numeric() {
+        let input_1 = String::from("1");
+        let mut lexer_1 = ElpLexer::new(input_1);
+        let tokens_1 = lexer_1.scan();
+        assert_eq!(
+            vec![
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 0,
+                        source: "".into()
+                    },
+                    token: LexerTokens::SOI
+                },
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 1,
+                        source: "1".into()
+                    },
+                    token: LexerTokens::Number("1".into())
+                }
+            ],
+            tokens_1
+        );
+
+        let input_123 = String::from("123");
+        let mut lexer_123 = ElpLexer::new(input_123);
+        let tokens_123 = lexer_123.scan();
+        assert_eq!(
+            vec![
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 0,
+                        source: "".into()
+                    },
+                    token: LexerTokens::SOI
+                },
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 3,
+                        source: "123".into()
+                    },
+                    token: LexerTokens::Number("123".into())
+                }
+            ],
+            tokens_123
+        );
+
+        let input_float = String::from("123.123");
+        let mut lexer_float = ElpLexer::new(input_float);
+        let tokens_float = lexer_float.scan();
+        assert_eq!(
+            vec![
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 0,
+                        source: "".into()
+                    },
+                    token: LexerTokens::SOI
+                },
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 7,
+                        source: "123.123".into()
+                    },
+                    token: LexerTokens::Number("123.123".into())
+                }
+            ],
+            tokens_float
+        );
+
+        let input_formatted = String::from("123_123");
+        let mut lexer_formatted = ElpLexer::new(input_formatted);
+        let tokens_formatted = lexer_formatted.scan();
+        assert_eq!(
+            vec![
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 0,
+                        source: "".into()
+                    },
+                    token: LexerTokens::SOI
+                },
+                SpannedToken {
+                    span: CodeSpan {
+                        start: 0,
+                        end: 7,
+                        source: "123_123".into()
+                    },
+                    token: LexerTokens::Number("123_123".into())
+                }
+            ],
+            tokens_formatted
+        )
     }
 }
--- crates/lexer/src/span.rs
+++ crates/lexer/src/span.rs
@@ -1,11 +1,13 @@
 use crate::tokens::LexerTokens;
 
+#[derive(Debug, PartialEq)]
 pub struct CodeSpan {
     pub start: usize,
     pub end: usize,
     pub source: String,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct SpannedToken {
     pub span: CodeSpan,
     pub token: LexerTokens,
